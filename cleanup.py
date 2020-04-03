45e7a775efc07750a61867b0b79d378c971052af
man so <chikman428@gmail.com>
2020-04-03 12:22:49 +0800

    ...

--- cleanup.py
+++ cleanup.py
@@ -8,12 +8,12 @@ def cleanup(arg):
     while True:
         hdd = psutil.disk_usage('/')
         space_mb = hdd.free//(1024*1024)
-        if space_mb < 1000:
+        if space_mb < 2000:
             print('Removing old image records')
             cwd = os.getcwd()
             files = glob.glob(os.path.join(cwd, 'record','*.*'))
             files.sort(key=os.path.getmtime)
-            print(len(files))
+            print('Number of files', len(files))
             print(files[0])
             for n in range(10000):
                 if len(files) > n:
